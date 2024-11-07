efca29e1acb27714d42577564537bd21a85bc1db
Lourival Lima Jr <37986945+lourivalplj@users.noreply.github.com>
2024-11-07 17:51:31 -0300

    Add project files.

---
+++ TallerAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TallerAPI.Services;
+
+namespace TallerAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        UserService userService = new UserService();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Get(string username)
+        {
+            var response = userService.GetUser(username);
+
+            if (response is null)
+                return Response.StatusCode  //NotFound
+            else
+                return //OK
+                   
+        }
+    }
+}
