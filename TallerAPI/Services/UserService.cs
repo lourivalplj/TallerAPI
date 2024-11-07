3ebb768584b7a3d81e1859e4a179aa2aa8662b9f
Lourival Lima Jr <37986945+lourivalplj@users.noreply.github.com>
2024-11-07 17:51:31 -0300

    Add project files.

---
+++ TallerAPI/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TallerAPI.Entities;
+
+namespace TallerAPI.Services
+{
+    public class UserService
+    {
+
+        public User GetUser(string username)
+        {
+
+        }
+    }
+}
